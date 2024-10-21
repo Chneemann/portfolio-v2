da3f7b6ca3a3bcecf1622a006251bec143cda71b
Chneemann <andre-kempf@mailbox.org>
2024-10-21 21:49:31 +0200

    feat: add and style SkillsComponent

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [LandingPageComponent, AboutMeComponent],
+  imports: [LandingPageComponent, AboutMeComponent, SkillsComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
 })
