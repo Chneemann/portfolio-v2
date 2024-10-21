ceb5115e4063744df4bebb4a79becdd1c158d435
Chneemann <andre-kempf@mailbox.org>
2024-10-21 21:36:02 +0200

    eat: add and style AboutMeComponent

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [LandingPageComponent],
+  imports: [LandingPageComponent, AboutMeComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
 })
