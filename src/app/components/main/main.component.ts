293f2e22beebb30be74c74c0ba13329945882bdd
Chneemann <andre-kempf@mailbox.org>
2024-10-22 04:58:53 +0200

    feat: create and integrate PortfolioComponent

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { SkillsComponent } from './skills/skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [LandingPageComponent, AboutMeComponent, SkillsComponent],
+  imports: [
+    LandingPageComponent,
+    AboutMeComponent,
+    SkillsComponent,
+    PortfolioComponent,
+  ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
 })
