b379aff6635cd48861b5ce6ce487045cbc9a575d
Chneemann <andre-kempf@mailbox.org>
2024-11-10 16:04:32 +0100

    refactor: extract social icons into separate component

--- src/app/components/main/landing-page/landing-page.component.ts
+++ src/app/components/main/landing-page/landing-page.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../../shared/components/button/button.component';
 import { TranslateDirective, TranslatePipe } from '@codeandweb/ngx-translate';
+import { SocialIconsComponent } from '../../../shared/components/social-icons/social-icons.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [ButtonComponent, TranslatePipe, TranslateDirective],
+  imports: [
+    ButtonComponent,
+    TranslatePipe,
+    TranslateDirective,
+    SocialIconsComponent,
+  ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss',
 })
--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -2,11 +2,18 @@ import { Component } from '@angular/core';
 import { LogoComponent } from '../logo/logo.component';
 import { RouterLink } from '@angular/router';
 import { TranslateDirective, TranslatePipe } from '@codeandweb/ngx-translate';
+import { SocialIconsComponent } from '../social-icons/social-icons.component';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [LogoComponent, RouterLink, TranslatePipe, TranslateDirective],
+  imports: [
+    LogoComponent,
+    RouterLink,
+    TranslatePipe,
+    TranslateDirective,
+    SocialIconsComponent,
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
