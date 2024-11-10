31ce545f0de6fc9e752287d0a39ed45506d00731
Chneemann <andre-kempf@mailbox.org>
2024-11-10 19:47:54 +0100

    refactor: improved performance and accessibility, optimized image paths, added aria-labels and alt text

--- src/app/components/main/skills/skills.component.ts
+++ src/app/components/main/skills/skills.component.ts
@@ -10,17 +10,17 @@ import { TranslatePipe } from '@codeandweb/ngx-translate';
 })
 export class SkillsComponent {
   skills = [
-    { src: './../../assets/img/skills/angular.svg', alt: 'angular' },
-    { src: './../../assets/img/skills/react.svg', alt: 'react' },
-    { src: './../../assets/img/skills/typescript.svg', alt: 'typescript' },
-    { src: './../../assets/img/skills/python.svg', alt: 'python' },
-    { src: './../../assets/img/skills/django.svg', alt: 'django' },
-    { src: './../../assets/img/skills/javascript.svg', alt: 'javascript' },
-    { src: './../../assets/img/skills/html.svg', alt: 'html' },
-    { src: './../../assets/img/skills/css.svg', alt: 'css' },
-    { src: './../../assets/img/skills/git.svg', alt: 'git' },
-    { src: './../../assets/img/skills/firebase.svg', alt: 'firebase' },
-    { src: './../../assets/img/skills/sql.svg', alt: 'sql' },
-    { src: './../../assets/img/skills/rest-api.svg', alt: 'rest-api' },
+    { src: 'assets/img/skills/angular.svg', alt: 'Angular framework' },
+    { src: 'assets/img/skills/react.svg', alt: 'React library' },
+    { src: 'assets/img/skills/typescript.svg', alt: 'TypeScript language' },
+    { src: 'assets/img/skills/python.svg', alt: 'Python language' },
+    { src: 'assets/img/skills/django.svg', alt: 'Django framework' },
+    { src: 'assets/img/skills/javascript.svg', alt: 'JavaScript language' },
+    { src: 'assets/img/skills/html.svg', alt: 'HTML markup language' },
+    { src: 'assets/img/skills/css.svg', alt: 'CSS style sheet language' },
+    { src: 'assets/img/skills/git.svg', alt: 'Git version control' },
+    { src: 'assets/img/skills/firebase.svg', alt: 'Firebase platform' },
+    { src: 'assets/img/skills/sql.svg', alt: 'SQL database language' },
+    { src: 'assets/img/skills/rest-api.svg', alt: 'REST API' },
   ];
 }
